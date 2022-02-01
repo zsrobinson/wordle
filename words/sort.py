32f357658fe7f6f3a33bbe6608d9bfb5c8db5317
Zachary Robinson <zachary_robinson@icloud.com>
2022-02-01 15:17:11 -0500

    Wordlists as json

--- words/sort.py
+++ words/sort.py
@@ -1,10 +1,14 @@
+import json
+
 length = int(input("letters? "))
+data = []
 
 with open("google-10000-english-usa-no-swears-medium.txt", "r") as source:
 	for line in source:
 		stripped_line = line.strip()
 		if len(stripped_line) == length:
 			print(stripped_line)
+			data.append(stripped_line)
 
-			with open(str(length) + "-letter-words.txt", 'a') as dest:
-				dest.write(stripped_line + "\n")
+with open(str(length) + "-letter-words.json", 'w') as dest:
+	dest.write(json.dumps(data))
